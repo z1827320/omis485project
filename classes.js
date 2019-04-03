8bf4b2660527e90bba9ebccd4143dbe070d39d3d
Dillon Larson <z1827320@students.niu.edu>
2019-04-03 00:02:06 -0500

    changes

--- classes.js
+++ classes.js
@@ -1,30 +1,30 @@
 window.onload = function () {
-    var classes = $("classes");
-    var h2_elements = classes.getElementsByTagName("h2");
-
+    var school = $("classes");
+    var h2_elements = school.getElementsByTagName("h2");
+    		    
     var headingNode;
     for (var i = 0; i < h2_elements.length; i++ ) {
-        headingNode = h2_elements[i];
-
-        //attach event handler
-        headingNode.onclick = function () {
-            var h2 = this; //h2 is current headingNode
-            if (h2.hasAtrribute("class")) {
-                h2.removeAttribute("class");
-            }
-            else {
-                h2.setAttribute("class", "minus");
-            }
-            if (h2.nextElementSibling.hasAttribute("class")) {
-                h2.nextElementSibling.removeAttribute("class");
-            }
-            else {
-                h2.nextElementSibling.setAttribute("class", "open");
-            }
+    	headingNode = h2_elements[i];
+    	
+    	// Attach event handler
+    	headingNode.onclick = function () {
+			var h2 = this;         // h2 is the current headingNode object
+			if (h2.hasAttribute("class")) {
+				h2.removeAttribute("class");	
+			}
+			else {
+				h2.setAttribute("class", "minus");
+			}
+			if (h2.nextElementSibling.hasAttribute("class")) {
+				h2.nextElementSibling.removeAttribute("class");
+			}
+			else {
+				h2.nextElementSibling.setAttribute("class", "open");
+			} 
         }
     }
 }
-
 var $ = function (id) {
-    return document.getElementById(id);
+	return document.getElementById(id);
 }
+faqs
